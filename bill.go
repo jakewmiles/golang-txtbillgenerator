cb7eead2ecf84271f6bc1dfe742f8067245b8b38
jakewmiles <jake.w.miles@gmail.com>
2021-10-20 21:00:20 +0100

    feat: newBill function

--- bill.go
+++ bill.go
@@ -5,3 +5,12 @@ type bill struct {
 	items map[string]float64
 	tip   float64
 }
+
+func newBill(name string) bill {
+	b := bill{
+		name:  name,
+		items: map[string]float64{},
+		tip:   0,
+	}
+	return b
+}
